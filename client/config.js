d2ec97714924c6d6753337d3c28c3ff3e079c50a
Otamq <shogo8160@icloud.com>
2025-05-26 23:42:54 +0900

    Update config.js

--- client/config.js
+++ client/config.js
@@ -1,9 +1,7 @@
 // src/config.js
 
 // 本番環境・開発環境で切り替える場合
-const BACKEND_URL = process.env.NODE_ENV === "production"
-  ? "https://dailyhackson2.onrender.com"
-  : "http://127.0.0.1:5000";
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "http://127.0.0.1:5000";
 
 export default {
   BACKEND_URL,
