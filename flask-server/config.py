b8cb40a70686b04220b6fc7aa1bb3dacc2b23bbc
Otamq <shogo8160@icloud.com>
2025-05-26 22:17:28 +0900

    Update config.py

--- flask-server/config.py
+++ flask-server/config.py
@@ -3,8 +3,7 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'instance', 'app.db')}"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.urandom(24)
-    
+    SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
     SCHEDULER_API_ENABLED = True
